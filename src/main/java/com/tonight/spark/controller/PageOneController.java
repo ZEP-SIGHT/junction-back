a7eb60426d140f58808684af6bda8199b8fc3928
Hoo <tbnsok40@gmail.com>
2022-12-01 02:14:36 +0900

    fix: PageOneController 코드 정리

--- src/main/java/com/tonight/spark/controller/PageOneController.java
+++ src/main/java/com/tonight/spark/controller/PageOneController.java
@@ -4,32 +4,22 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.tonight.spark.dto.PageOneDto;
 import com.tonight.spark.service.Page1Service;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(PageOneController.URL_PREFIX)
+@RequestMapping("api/v1/page1")
 @RequiredArgsConstructor
 @CrossOrigin(origins = {"https://tonightspark-frontend.pages.dev", "http://localhost:3000"})
-public class PageOneController extends RestControllerBase{
-    static final String URL_PREFIX = API_PREFIX + "/page1";
-
+public class PageOneController extends RestControllerBase {
     private final Page1Service page1Service;
 
-    @RequestMapping(
-            value = "/{mapHash}",
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
+    @GetMapping("/{mapHash}")
     @ResponseBody
-    public ResponseEntity<List<PageOneDto>> getStatisForAuth(@PathVariable String mapHash) throws JsonProcessingException {
-
-
+    public ResponseEntity<List<PageOneDto>> getAuthStatistic(@PathVariable String mapHash) throws JsonProcessingException {
         List<PageOneDto> dataForAuth = page1Service.getDataForAuth(mapHash);
-        //page1Service.test();
         return ResponseEntity.ok().body(dataForAuth);
     }
 
