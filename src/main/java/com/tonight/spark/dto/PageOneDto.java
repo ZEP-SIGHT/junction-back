4f137ae09803dd1f779278e2af979c6484f8d2be
Hoo <tbnsok40@gmail.com>
2022-12-02 20:21:32 +0900

    refactoring: PageOneDto 스펙 수정 및 AreaTimeCount 재정의

--- src/main/java/com/tonight/spark/dto/PageOneDto.java
+++ src/main/java/com/tonight/spark/dto/PageOneDto.java
@@ -1,18 +1,20 @@
 package com.tonight.spark.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import java.util.HashMap;
 import java.util.List;
 
 @Data
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class PageOneDto {
-    public String authorityName;
-    public HashMap<String, List<TimeCount>> mapData;
+    public String authority;
+    public List<AreaTimeCount> areas;
+
+    public static PageOneDto create(String auth, List<AreaTimeCount> areas) {
+        return PageOneDto.builder()
+                .authority(auth)
+                .areas(areas)
+                .build();
+    }
 }
