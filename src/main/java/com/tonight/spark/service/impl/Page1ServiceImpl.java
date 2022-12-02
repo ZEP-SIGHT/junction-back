a706cb617f67bf962e245165e6e783e615e8ca6c
Hoo <tbnsok40@gmail.com>
2022-12-02 20:22:48 +0900

    refactoring: 리팩토링 api

--- src/main/java/com/tonight/spark/service/impl/Page1ServiceImpl.java
+++ src/main/java/com/tonight/spark/service/impl/Page1ServiceImpl.java
@@ -2,87 +2,50 @@ package com.tonight.spark.service.impl;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.tonight.spark.domain.StayTime;
-import com.tonight.spark.domain.Visited;
+import com.tonight.spark.dto.AreaTimeCount;
 import com.tonight.spark.dto.PageOneDto;
-import com.tonight.spark.dto.TimeCount;
-import com.tonight.spark.dto.page1.AuthType;
 import com.tonight.spark.repository.StayTimeRepository;
-import com.tonight.spark.repository.VisitedRepository;
 import com.tonight.spark.service.Page1Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.*;
+
+import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.toList;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class Page1ServiceImpl implements Page1Service {
 
-    private final VisitedRepository visitedRepository;
-
     private final StayTimeRepository stayTimeRepository;
 
-    @Override
     public List<PageOneDto> getDataForAuth(String mapHash) throws JsonProcessingException {
 
-        List<PageOneDto> finalResult = new ArrayList<>();
-        List<StayTime> stayTimeByMapHash = stayTimeRepository.findStayTimeByMapHash(mapHash);
-        List<Visited> VisitByMapHash = visitedRepository.findVisitedByMapHash(mapHash);
+        List<StayTime> stayTimes = stayTimeRepository.findStayTimeByMapHash(mapHash);
+        Map<String, Map<String, List<Integer>>> nearResult = stayTimes.stream().collect(
+                groupingBy(StayTime::getPlayerAuth,
+                        groupingBy(StayTime::getAreaName, mapping(StayTime::getDuration, toList()))
+                )
+        );
 
-        Set<String> areaSet = stayTimeByMapHash.stream().map(data -> data.getAreaName()).collect(Collectors.toSet());
+        return nearResult.entrySet().stream()
+                .map(data -> PageOneDto.create(data.getKey(), getAreaTimeCounts(data.getValue())))
+                .collect(toList());
+    }
 
-        AuthType[] values = AuthType.values();
-        HashMap<AuthType, List<StayTime>> authorityMap = new HashMap<>();
+    private static List<AreaTimeCount> getAreaTimeCounts(Map<String, List<Integer>> byAuth) {
 
-        for (AuthType value : values) {
-            authorityMap.put(value, new ArrayList<>());
-        }
-        for (AuthType authType : authorityMap.keySet()) {
-            List<StayTime> collect = stayTimeByMapHash.stream().filter(data -> data.getPlayerAuth().equals(authType.getCode())).collect(Collectors.toList());
-            authorityMap.put(authType, collect);
-        }
-
-        System.out.println(authorityMap);
-
-        for (AuthType authType : authorityMap.keySet()) { // 6
-            log.info("area : {}", areaSet.size());
-            HashMap<String, List<TimeCount>> areaHashMap = new HashMap<>();
-            areaSet.stream().forEach(area -> areaHashMap.put(area, new ArrayList<>()));
-
-            log.info("area : {} {}", areaHashMap.size(), areaHashMap);
-            for (String area : areaSet) { // 9
-                List<StayTime> collect = stayTimeByMapHash.stream().filter(stayTime -> stayTime.getAreaName().equals(area))
-                        .filter(data -> data.getPlayerAuth().equals(authType.getCode()))
-                        .collect(Collectors.toList());
-                List<Visited> conditionVisited = VisitByMapHash.stream().filter(visited -> visited.getAreaName().equals(area))
-                        .filter(data -> data.getPlayerAuth().equals(authType.getCode()))
-                        .collect(Collectors.toList());
-
-                int totalStayTime = 0;
-                for (StayTime data : collect) {
-                    totalStayTime += Integer.parseInt(data.getDuration());
-                }
-                log.info("visit, stytime : {}", conditionVisited.size() + "," + totalStayTime);
-                areaHashMap.get(area).add(new TimeCount(conditionVisited.size(),totalStayTime));
-            }
-            System.out.println(areaHashMap);
-            PageOneDto build = PageOneDto.builder()
-                    .authorityName(authType.getCode())
-                    .mapData(areaHashMap)
-                    .build();
-            finalResult.add(build);
-        }
-        return finalResult;
+        return byAuth.entrySet()
+                .stream()
+                .map(area -> AreaTimeCount.create(area.getKey(), area.getValue()))
+                .collect(toList());
     }
 
     @Override
     public void test() {
-        log.info("test : {}",stayTimeRepository.getStayTimeSumForGroup("NONE", "3001", "DKMaPm"));
+        log.info("test : {}", stayTimeRepository.getStayTimeSumForGroup("NONE", "3001", "DKMaPm"));
     }
 }
